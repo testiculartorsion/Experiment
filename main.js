5f82162987fddf9eee75a94ecc60e40aa209a9d7
testiculartorsion <albertheruns1@hotmail.com>
2019-03-28 23:18:07 -0400

    Update main.js

--- main.js
+++ main.js
@@ -49,10 +49,10 @@ $(document).ready(function(){
             ball.attr("cy","400")
             ball.attr("vy","0")
         }
-		if (parseInt(ball.attr("cx"))<100){
+	if (parseInt(ball.attr("cx"))<100){
 			ball.attr("cx", "100")
 			ball.attr("vx", "0")
-		}
+	}
 		
         //passives forces (friction and gravity)
         if(!mouseDown){
